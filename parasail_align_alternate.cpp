14102281c68152cd13201d67681746a710437972
Sabiq Chaudhary <schaudhary@oicr.on.ca>
2020-07-28 11:12:41 -0400

    Cleaned up the code

--- parasail_align_alternate.cpp
+++ parasail_align_alternate.cpp
@@ -1,5 +1,3 @@
-
-
 #include <zlib.h>
 #include <stdio.h>
 #include "kseq.h"
@@ -16,13 +14,14 @@
 #include <iomanip>
 #include <algorithm>
 #include <unordered_set>
+#include <chrono>
 
 KSEQ_INIT(gzFile, gzread)
 
 //std::unordered_map<std::string,std::string> control_substrings;
 //std::unordered_map<std::string,int> test_substrings;
 
-void subString_control(std::string& s, int sub_length,std::string& probe_name, auto& control_substrings)  { 
+inline void subString_control(const std::string& s, int sub_length,const std::string& probe_name, auto& control_substrings)  { 
     //std::unordered_map<std::string,int> temp_array;
     int n = s.length();
     //#pragma omp parallel for
@@ -34,15 +33,16 @@ void subString_control(std::string& s, int sub_length,std::string& probe_name, a
     //control_substrings = temp_array;
 } 
 
-void subString_test(std::string& s, int sub_length, auto& test_substrings)  {
+inline void subString_test(const std::string& s, int sub_length, auto& test_substrings)  {
     int n = s.length();
+    //#pragma omp parallel for
     for (int i = 0; i < n; i++)  
         if(i+sub_length < n) {
             test_substrings.insert({s.substr(i, sub_length), 1});
         }
 } 
 
-bool find_substring(std::string to_find, auto& test_substrings) {
+inline bool find_substring(const std::string& to_find, auto& test_substrings) {
     auto got = test_substrings.find (to_find);
 
     if ( got == test_substrings.end() ) {
@@ -85,16 +85,12 @@ int main(int argc, char *argv[])  {
 	
     parasail_result_t *best_result = NULL;
     parasail_matrix_t *matrix = NULL;
-    //parasail_traceback_t  *best_traceback = NULL;
-    //parasail_cigar_t *cigar;
+    
     std::string best_query,best_ref,best_comp,best_oligo;
     std::string second_best_query,second_best_ref,second_best_comp,second_best_oligo;
 
 
     matrix = parasail_matrix_create("ACGT", 5, -1);	
-    //EdlibAlignResult result;
-    //FILE *out_file;
-    //out_file = fopen("traceback_output.txt", "w");
 
     int read_aligns;
 
@@ -109,24 +105,23 @@ int main(int argc, char *argv[])  {
     getline(control_fd, one_line);
     while(control_fd) {
         getline(control_fd,one_line);
-        //std::cout<<one_line<<std::endl;
+        
 	std::string non_rep_section;
         if(one_line.length() > 75) {
-            sequence2 = one_line.substr(one_line.find('A'));
-	    non_rep_section = sequence2.substr(61,68);
-	    //std::cout<<non_rep_section<<std::endl;
-            std::string temp = one_line.substr(one_line.find('F'));
+            sequence2 = one_line.substr(one_line.find('A')); //used to separate the sequence from the probe no and name
+	    non_rep_section = sequence2.substr(61,68);   //indexed used to extract the non repeating sequence from the sequence obtained above
+	    
+            std::string temp = one_line.substr(one_line.find('F')); //used to extract the probe name
             probe_name = temp.substr(0,temp.find('A'));
         }
-        //std::cout<<"Calculating "<<c<<"....\n";
+        
         c++;
         subString_control(non_rep_section, k, sequence2, control_substrings);
     }
     std::cout<<"Calculated "<<c<<" control sequence.....\n";
-    //int max = 0, second_max = 0,
+    
     c = 0;
     while ((l = kseq_read(seq)) >= 0) {
-        //std::cout<<seq->name.s<<std::endl<<seq->seq.s<<std::endl;
 	max = 0;
 	second_max = 0;
         std::string sequence(seq->seq.s);
@@ -139,27 +134,25 @@ int main(int argc, char *argv[])  {
 
         while (itr != control_substrings.end()) {                    
             if(find_substring(itr->first,test_substrings) == true) {
-                //std::cout<<"Found\n";
+                
                 /*if(std::find(control_set.begin(), control_set.end(), itr->second) == control_set.end()) {
                     control_set.push_back(itr->second);
                 }*/
 		control_set.insert(itr->second);
-            } else {
-                    //std::cout<<"Not Found\n";
-            }
+            } 
             itr++;
         }
 	read_aligns = 0;
-        //for(auto i = 0; i<control_set.size();i++) {
+	
 	for(const auto& elem: control_set){
-            //std::cout<<control_set[i]<<std::endl;
-            //call parasail here
-	    char seq_arr[elem.length() + 1];
-            strcpy(seq_arr,elem.c_str());
-
-            parasail_result_t *result = parasail_sg_trace_scan_16(seq->seq.s,sequence.length(),seq_arr,elem.length(),5,4,matrix);
-            parasail_traceback_t *traceback = parasail_result_get_traceback(result,seq->seq.s, sequence.length(), seq_arr, elem.length(),matrix,'|','*','*');
-            if(result->score > max) {
+	    //char seq_arr[elem.length() + 1];
+            //strcpy(seq_arr,elem.c_str());
+	    //auto t1 = std::chrono::high_resolution_clock::now();
+            parasail_result_t *result = parasail_sg_trace_scan_16(seq->seq.s,sequence.length(),elem.c_str(),elem.length(),5,4,matrix);
+            //auto t2 = std::chrono::high_resolution_clock::now();
+	    parasail_traceback_t *traceback = parasail_result_get_traceback(result,seq->seq.s, sequence.length(), elem.c_str(), elem.length(),matrix,'|','*','*');
+            //auto t3 = std::chrono::high_resolution_clock::now();
+	    if(result->score > max) {
                 second_max = max;
                 max = result->score;
                 //best_result = result;
@@ -177,8 +170,13 @@ int main(int argc, char *argv[])  {
 		best_oligo = elem;
                 //std::cout<<traceback->ref<<std::endl;
             }
+	    //auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+
+	    //auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>( t3 - t2 ).count();
+	
+	    //std::cout<<"Result Time: "<<duration1<<"\nTraceback Time: "<<duration2<<std::endl;
 	    read_aligns++;
-            //parasail_traceback_free(traceback);
+            parasail_traceback_free(traceback);
             parasail_result_free(result);
         }
         //std::cout<<"Calculating sequence "<<c<<".....\n";
@@ -201,102 +199,6 @@ int main(int argc, char *argv[])  {
         output_file<<second_best_query<<"\n"<<second_best_comp<<"\n"<<second_best_ref<<"\n\n";*/
     }
 
-	//std::cout<<"testing1\n";
-	//std::unordered_map<std::string,std::string>::iterator itr = control_substrings.begin();
-
-        //while (itr != control_substrings.end()) {
-            //std::cout<<"testing:"<<itr->second<<std::endl;
-            //itr++;
-        //}
-
-	/*
-	while ((l = kseq_read(seq)) >= 0) {
-		
-		std::string sequence(seq->seq.s);
-		char *fastq_seq = seq->seq.s;
-		//int c = 0;
-		int max = 0;
-		int second_max = 0;
-		
-		subString_test(sequence, sequence.length(), k);
-	
-		control_fd.clear();
-		control_fd.seekg(0,std::ios::beg);
-		getline(control_fd,one_line);
-		while(control_fd) {
-			getline(control_fd,one_line);
-			sequence2 = removeDupWord(one_line);
-			if(one_line.length() > 75)
-				sequence2 = one_line.substr(one_line.find('A'));
-			
-			//std::cout<<sequence2<<std::endl;
-			subString_control(sequence2, sequence2.length(), k);
-
-			char seq_arr[sequence2.length() + 1];
-			strcpy(seq_arr,sequence2.c_str());
-			if(sequence2.length() == 0)
-				break;
-			std::unordered_map<std::string,int>::iterator itr = test_substrings.begin();
-
-			while (itr != test_substrings.end()) {
-				//printf("testing4");
-				//std::cout<<itr->first<<std::endl;
-				if(find_substring(itr->first) == true) {
-					//std::cout<<itr->first<<" Found\n";
-					//printf("testing2");
-					//c++;
-				//	std::cout<<c<<" "<<sequence2.length()<<std::endl;
-					//std::cout<<seq->seq.s<<std::endl;
-					//std::cout<<seq_arr<<std::endl;
-					parasail_result_t *result = parasail_sg_trace_scan_sat(fastq_seq,sequence.length(),seq_arr,sequence2.length(),5,4,matrix);
-					parasail_traceback_t *traceback = parasail_result_get_traceback(result,seq->seq.s, sequence.length(), seq_arr, sequence2.length(),matrix,'|','*','*');
-					//parasail_traceback_generic_extra(fastq_seq, sequence.length(), seq_arr, sequence2.length(), "Query: ","Target: ", matrix, result, '|','*','*',60,7,1,8,out_file);
-					//cigar = parasail_result_get_cigar(result, fastq_seq,sequence.length(), seq_arr, sequence2.length(), matrix);
-					//char *cigar_decoded = parasail_cigar_decode(cigar);
-					//std::cout<<traceback->query<<std::endl;
-					if(result->score > max) {
-						second_max = max;
-						max = result->score;
-						best_result = result;
-						best_ref = traceback ->ref;
-						best_comp = traceback->comp;
-						best_query = traceback->query;
-					} 
-
-					parasail_result_free(result);
-					parasail_traceback_free(traceback);
-					//std::cout<< traceback->query<<"\n"<<traceback->comp<<"\n"<<traceback->ref<<std::endl;
-					/*result = edlibAlign(seq->seq.s, sequence.length(), seq_arr, sequence2.length(), edlibNewAlignConfig(-1, EDLIB_MODE_HW, EDLIB_TASK_PATH, NULL, 0));
-					if (result.status == EDLIB_STATUS_OK) {
-						if(result.editDistance>max > 0)
-							max = result.editDistance;
-					}
-					break;
-				}
-					//Call Parasail here!!
-				//else {
-					//std::cout<<itr->first<<" Not found\n";
-					//printf("testing3");
-				//}
-				itr++;
-			}
-
-			control_substrings.erase(control_substrings.begin(),control_substrings.end());
-			//std::cout<<one_line;
-			//std::cout<<probe_no<<std::endl<<probe_name<<std::endl<<seq<<std::endl<<one_line;
-			//std::cout<<"seq "<<seq<<"\nProbe_no "<<probe_no<<"\n Probe name "<<probe_name<<std::endl;
-			//std::cout<<"Found matching current test_sequence "<<c<<std::endl;
-			//break;
-			
-		}
-		test_substrings.erase(test_substrings.begin(), test_substrings.end());
-		std::cout<<"Highest Score: "<<max<<"\nSecond Highest Score:"<<second_max<<std::endl;
-		std::cout<< best_query<<"\n"<<best_comp<<"\n"<<best_ref<<std::endl;
-
-
-	}*/
-	//parasail_traceback_free(traceback);
-	//parasail_result_free(result);
     parasail_matrix_free(matrix);
     //fclose(out_file);
     control_fd.close();
