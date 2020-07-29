8a3e730a690712c96d81f9cbb6a70a6a0f9fb608
Sabiq Chaudhary <schaudhary@oicr.on.ca>
2020-07-29 15:25:45 -0400

    Output formatting changed

--- parasail_align_alternate.cpp
+++ parasail_align_alternate.cpp
@@ -86,7 +86,7 @@ int main(int argc, char *argv[])  {
     parasail_result_t *best_result = NULL;
     parasail_matrix_t *matrix = NULL;
     
-    std::string best_query,best_ref,best_comp,best_oligo;
+    std::string best_query,best_ref,best_comp,best_oligo, best_probe_name;
     std::string second_best_query,second_best_ref,second_best_comp,second_best_oligo;
 
 
@@ -116,10 +116,12 @@ int main(int argc, char *argv[])  {
         }
         
         c++;
-        subString_control(non_rep_section, k, sequence2, control_substrings);
+        subString_control(non_rep_section, k, one_line, control_substrings);
     }
     std::cout<<"Calculated "<<c<<" control sequence.....\n";
     
+    std::cout<<std::left<<std::setw(55)<<"Sequence Name"<<std::setw(35)<<"Oligo Name"<<std::setw(35)<<"Reads Aligned"<<std::setw(35)<<"Score"<<std::endl;
+
     c = 0;
     while ((l = kseq_read(seq)) >= 0) {
 	max = 0;
@@ -145,13 +147,16 @@ int main(int argc, char *argv[])  {
 	read_aligns = 0;
 	
 	for(const auto& elem: control_set){
-	    //char seq_arr[elem.length() + 1];
-            //strcpy(seq_arr,elem.c_str());
-	    //auto t1 = std::chrono::high_resolution_clock::now();
-            parasail_result_t *result = parasail_sg_trace_scan_16(seq->seq.s,sequence.length(),elem.c_str(),elem.length(),5,4,matrix);
-            //auto t2 = std::chrono::high_resolution_clock::now();
-	    parasail_traceback_t *traceback = parasail_result_get_traceback(result,seq->seq.s, sequence.length(), elem.c_str(), elem.length(),matrix,'|','*','*');
-            //auto t3 = std::chrono::high_resolution_clock::now();
+	    
+	    sequence2 = elem.substr(elem.find('A')); //used to separate the sequence from the probe no and name
+	      
+            std::string temp = elem.substr(elem.find('F')); //used to extract the probe name
+            probe_name = temp.substr(0,temp.find('A'));
+
+            parasail_result_t *result = parasail_sg_trace_scan_16(seq->seq.s,sequence.length(),sequence2.c_str(),sequence2.length(),5,4,matrix);
+
+	    parasail_traceback_t *traceback = parasail_result_get_traceback(result,seq->seq.s, sequence.length(), sequence2.c_str(), sequence2.length(),matrix,'|','*','*');
+
 	    if(result->score > max) {
                 second_max = max;
                 max = result->score;
@@ -159,7 +164,7 @@ int main(int argc, char *argv[])  {
                 //second_best_probe_no = best_probe_no;
                 //second_best_probe_name = best_probe_name;
                 //best_probe_no = probe_no;
-                //best_probe_name = probe_name;
+                best_probe_name = probe_name;
                 second_best_query = best_query;
                 second_best_ref = best_ref;
                 second_best_comp = best_comp;
@@ -185,12 +190,14 @@ int main(int argc, char *argv[])  {
 	//output_file<<std::left<<std::setw(55)<<seq->name.s<<std::setw(35)<<best_probe_name<<std::setw(35)<<max<<std::setw(35)<<second_best_probe_name<<std::setw(35)<<second_max<<std::endl;
         c++;
 
-	std::cout<<"\n\n\nSequence "<<c<<" : "<<seq->seq.s;
-	std::cout<<"\nAligned to: "<<read_aligns<<" control oligos";
-        std::cout<<"\nHighest Score: "<<max<<"\nControl Oligo: "<<best_oligo<<"\n\n";
-        std::cout<<best_query<<"\n"<<best_comp<<"\n"<<best_ref;
-        std::cout<<"\n\nSecond Highest Score:"<<second_max<<"\nControl Oligo: "<<second_best_oligo<<"\n\n";
-        std::cout<<second_best_query<<"\n"<<second_best_comp<<"\n"<<second_best_ref<<"\n\n";
+	//std::cout<<"\n\n\nSequence "<<c<<" : "<<seq->seq.s;
+	//std::cout<<"\nAligned to: "<<read_aligns<<" control oligos";
+        //std::cout<<"\nHighest Score: "<<max<<"\nControl Oligo: "<<best_oligo<<"\n\n";
+        //std::cout<<best_query<<"\n"<<best_comp<<"\n"<<best_ref;
+        //std::cout<<"\n\nSecond Highest Score:"<<second_max<<"\nControl Oligo: "<<second_best_oligo<<"\n\n";
+        //std::cout<<second_best_query<<"\n"<<second_best_comp<<"\n"<<second_best_ref<<"\n\n";
+
+	std::cout<<std::left<<std::setw(55)<<seq->name.s<<std::setw(35)<<best_probe_name<<std::setw(35)<<read_aligns<<std::setw(35)<<max<<std::endl;	
 
 	/*output_file<<"\n\n\nSequence "<<c<<" : "<<seq->seq.s;
         output_file<<"\nHighest Score: "<<max<<"\nProbe Name:"<<best_probe_no;
