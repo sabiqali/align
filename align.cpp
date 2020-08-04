155b8fb75dbf74052b28a1cd6b588c2b7789ba77
sabiqali <sabiq.work@gmail.com>
2020-08-04 14:37:54 -0700

    Command Line options added

--- align.cpp
+++ align.cpp
@@ -181,18 +181,64 @@ std::string removeDupWord(std::string str)  {
    return word;
 }
 
+void printAlignment(const char* query, const char* target,
+                    const unsigned char* alignment, const int alignmentLength,
+                    const int position, const EdlibAlignMode modeCode) {
+    int tIdx = -1;
+    int qIdx = -1;
+    if (modeCode == EDLIB_MODE_HW) {
+        tIdx = position;
+        for (int i = 0; i < alignmentLength; i++) {
+            if (alignment[i] != EDLIB_EDOP_INSERT)
+                tIdx--;
+        }
+    }
+    for (int start = 0; start < alignmentLength; start += 50) {
+        // target
+        printf("T: ");
+        int startTIdx = -1;
+        for (int j = start; j < start + 50 && j < alignmentLength; j++) {
+            if (alignment[j] == EDLIB_EDOP_INSERT)
+                printf("-");
+            else
+                printf("%c", target[++tIdx]);
+            if (j == start)
+                startTIdx = tIdx;
+        }
+        printf(" (%d - %d)\n", std::max(startTIdx, 0), tIdx);
+
+        // match / mismatch
+        printf("   ");
+        for (int j = start; j < start + 50 && j < alignmentLength; j++) {
+            printf(alignment[j] == EDLIB_EDOP_MATCH ? "|" : " ");
+        }
+        printf("\n");
+
+        // query
+        printf("Q: ");
+        int startQIdx = qIdx;
+        for (int j = start; j < start + 50 && j < alignmentLength; j++) {
+            if (alignment[j] == EDLIB_EDOP_DELETE)
+                printf("-");
+            else
+                printf("%c", query[++qIdx]);
+            if (j == start)
+                startQIdx = qIdx;
+        }
+        printf(" (%d - %d)\n\n", std::max(startQIdx, 0), qIdx);
+    }
+}
+
 int main(int argc, char *argv[])  {
     gzFile fp;
     kseq_t *seq;
     int l;
-    if (argc != 3) {
-    	fprintf(stderr, "Usage: %s reads.fastq control_oligos.txt\n", argv[0]);
-	    return 1;
-    }
 
-    fp = gzopen(argv[1], "r");
+    parse_align_options(argc , argv);
+
+    fp = gzopen(opt::reads_file.c_str(), "r");
     seq = kseq_init(fp);
-    std::ifstream control_fd(argv[2]);
+    std::ifstream control_fd(opt::control_file);
 	
     std::unordered_map<std::string,std::string> control_substrings;
 	
@@ -232,6 +278,10 @@ int main(int argc, char *argv[])  {
     std::cerr << "Read " << control_count << " control sequences\n";
     std::cout << "read_name\toligo_name\tnum_alignments\tbest_sccore" << std::endl;
 
+    unsigned char * best_alignment, second_best_alignment;
+    int best_alignmentLength, second_best_alignmentLength;
+    int* best_endLocations, second_best_endLocations;
+
     int read_count = 0;
     while ((l = kseq_read(seq)) >= 0) {
 	    max = 0;
@@ -255,45 +305,66 @@ int main(int argc, char *argv[])  {
             std::string sequence2 = elem.substr(elem.find('A')); //used to separate the sequence from the probe no and name
             std::string temp = elem.substr(elem.find('F')); //used to extract the probe name
             probe_name = temp.substr(0,temp.find('A'));
-            parasail_result_t* result = parasail_sg_trace_scan_16(seq->seq.s,sequence.length(),sequence2.c_str(),sequence2.length(),5,4,matrix);
-            parasail_traceback_t* traceback = parasail_result_get_traceback(result,seq->seq.s, sequence.length(), sequence2.c_str(), sequence2.length(),matrix,'|','*','*');
+            if(opt::parasail) {
+                parasail_result_t* result = parasail_sg_trace_scan_16(seq->seq.s,sequence.length(),sequence2.c_str(),sequence2.length(),5,4,matrix);
+                parasail_traceback_t* traceback = parasail_result_get_traceback(result,seq->seq.s, sequence.length(), sequence2.c_str(), sequence2.length(),matrix,'|','*','*');
 
-            if(result->score > max) {
-                second_max = max;
-                max = result->score;
-                best_probe_name = probe_name;
-                second_best_query = best_query;
-                second_best_ref = best_ref;
-                second_best_comp = best_comp;
-                best_ref = traceback ->ref;
-                best_comp = traceback->comp;
-                best_query = traceback->query;
-                second_best_oligo = best_oligo;
-                best_oligo = elem;
+                if(result->score > max) {
+                    second_max = max;
+                    max = result->score;
+                    best_probe_name = probe_name;
+                    second_best_query = best_query;
+                    second_best_ref = best_ref;
+                    second_best_comp = best_comp;
+                    best_ref = traceback ->ref;
+                    best_comp = traceback->comp;
+                    best_query = traceback->query;
+                    second_best_oligo = best_oligo;
+                    best_oligo = elem;
+                }
+                parasail_traceback_free(traceback);
+                parasail_result_free(result);
             }
-
-    	    //auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-	        //auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>( t3 - t2 ).count();
-	        //std::cout<<"Result Time: "<<duration1<<"\nTraceback Time: "<<duration2<<std::endl;
+            if(opt::edlib) {
+                EdlibAlignResult result = edlibAlign(seq->seq.s, sequence.length(), sequence2.c_str(), sequence2.length(), edlibNewAlignConfig(-1, EDLIB_MODE_HW, EDLIB_TASK_PATH, NULL, 0));
+	    
+                if(result.editDistance > max) {
+                    second_max = max;
+                    max = result.editDistance;
+                    best_probe_name = probe_name;
+                    second_best_oligo = best_oligo;
+                    best_oligo = sequence2;
+                    second_best_alignment = best_alignment;
+                    best_alignment = result.alignment;
+                    second_best_alignmentLength = best_alignmentLength;
+                    best_alignmentLength = result.alignmentLength;
+                    second_best_endLocations = best_endLocations;
+                    best_endLocations = result.endLocations;
+                }
+                edlibFreeAlignResult(result);
+            }
+    	    
 	        num_alignments++;
-            
-            parasail_traceback_free(traceback);
-            parasail_result_free(result);
-        }
-        //std::cout<<"Calculating sequence "<<c<<".....\n";
-        //std::cout<<"Highest Score:"<<max<<std::endl;
-        //std::cout<<"Second Highest Score:"<<second_max<<std::endl;
-        //output_file<<std::left<<std::setw(55)<<seq->name.s<<std::setw(35)<<best_probe_name<<std::setw(35)<<max<<std::setw(35)<<second_best_probe_name<<std::setw(35)<<second_max<<std::endl;
+        }   
+        
         read_count++;
 
-        //std::cout<<"\n\n\nSequence "<<c<<" : "<<seq->seq.s;
-        //std::cout<<"\nAligned to: "<<read_aligns<<" control oligos";
-        //std::cout<<"\nHighest Score: "<<max<<"\nControl Oligo: "<<best_oligo<<"\n\n";
-        //std::cout<<best_query<<"\n"<<best_comp<<"\n"<<best_ref;
-            //std::cout<<"\n\nSecond Highest Score:"<<second_max<<"\nControl Oligo: "<<second_best_oligo<<"\n\n";
-            //std::cout<<second_best_query<<"\n"<<second_best_comp<<"\n"<<second_best_ref<<"\n\n";
-
-        std::cout << std::left << seq->name.s << "\t" << best_probe_name << "\t" << num_alignments << "\t" << max << std::endl;	
+        if(opt::print_alignment) {
+            std::cout<<"\n\n\nSequence "<<c<<" : "<<seq->seq.s;
+            std::cout<<"\nAligned to: "<<read_aligns<<" control oligos";
+            std::cout<<"\nHighest Score: "<<max<<"\nControl Oligo: "<<best_oligo<<"\n\n";
+            if(opt::parasail)
+                std::cout<<best_query<<"\n"<<best_comp<<"\n"<<best_ref;
+            if(opt::edlib)
+                printAlignment(seq->seq.s, best_oligo.c_str(), best_alignment, best_alignmentLength, *(best_endLocations), EDLIB_MODE_NW);
+            std::cout<<"\n\nSecond Highest Score:"<<second_max<<"\nControl Oligo: "<<second_best_oligo<<"\n\n";
+            if(opt::parasail)
+                std::cout<<second_best_query<<"\n"<<second_best_comp<<"\n"<<second_best_ref<<"\n\n";
+            if(opt::edlib)
+                printAlignment(seq->seq.s, second_best_oligo.c_str(), second_best_alignment, second_best_alignmentLength, *(second_best_endLocations), EDLIB_MODE_NW);
+        }
+        else
+            std::cout << std::left << seq->name.s << "\t" << best_probe_name << "\t" << num_alignments << "\t" << max << std::endl;	
 
 	    /*output_file<<"\n\n\nSequence "<<c<<" : "<<seq->seq.s;
         output_file<<"\nHighest Score: "<<max<<"\nProbe Name:"<<best_probe_no;
